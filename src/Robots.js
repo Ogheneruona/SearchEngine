67bc0b746b0f8b84e70ffced1bf53edf0022ee0c
Ogheneruona <ruon85@gmail.com>
2019-12-04 10:57:30 +0000

    Search page

---
+++ src/Robots.js
@@ -0,0 +1,59 @@
+export const Robots = [
+{
+id: 1,
+name: 'Terminator',
+username: 'Arnold Schazenwigher',
+email: '$1000 USD'
+},
+{
+id: 2,
+name: 'Robocop',
+username: 'Bruce lee',
+email: '$800 USD'
+},
+{
+id: 3,
+name: 'Robot boy',
+username: 'Will Pharell',
+email: '$300 USD'
+},
+{
+id: 4,
+name: 'Megas XLR',
+username: 'Jamie Coop',
+email: '$500 USD'
+},
+{
+id: 5,
+name: 'Star trek',
+username: 'geme foo',
+email: '$500 USD'
+},{
+id: 6,
+name: 'Interstellar',
+username: 'Jamie Coop',
+email: '$500 USD'
+},{
+id: 7,
+name: 'Iron man',
+username: 'Jamie Coop',
+email: '$500 USD'
+},{
+id: 8,
+name: 'The Matrix',
+username: 'Jamie Coop',
+email: '$500 USD'
+},{
+id: 9,
+name: 'Mortal engine',
+username: 'Jamie Coop',
+email: '$500 USD'
+},{
+id: 10,
+name: 'Bumble bee',
+username: 'Jamie Coop',
+email: '$500 USD'
+},
+]
+
+export default Robots;
